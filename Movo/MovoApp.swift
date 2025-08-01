dca2d9929320f596349032b095e7a1e61ebc36ee
Benedikt Purkott <benediktpurkott@gmail.com>
2025-08-01 23:58:33 +0200

    Initial Commit

---
+++ Movo/MovoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovoApp.swift
+//  Movo
+//
+//  Created by Benedikt on 01.08.25.
+//
+
+import SwiftUI
+
+@main
+struct MovoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovoTests/MovoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovoTests.swift
+//  MovoTests
+//
+//  Created by Benedikt on 01.08.25.
+//
+
+import Testing
+@testable import Movo
+
+struct MovoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
